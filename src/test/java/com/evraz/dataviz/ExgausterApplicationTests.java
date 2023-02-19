9ec73d75716d934adfd46068baffe2ea44f311ea
el-hopaness-romtic <60661917+el-hopaness-romtic@users.noreply.github.com>
2023-02-19 04:59:40 +0300

    testcontainers с postgres

--- src/test/java/com/evraz/dataviz/ExgausterApplicationTests.java
+++ src/test/java/com/evraz/dataviz/ExgausterApplicationTests.java
@@ -2,7 +2,11 @@ package com.evraz.dataviz;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@ActiveProfiles("test")
+@Testcontainers
 @SpringBootTest
 class ExgausterApplicationTests {
 
